90519664d653ca7a20834801e172ac94d80da2a6
Eminence <himanshumaurya447756@gmail.com>
2026-06-19 07:20:39 +0000

    added.

---
+++ Arrays/JumpProblem2.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class JumpProblem2 {
+      public int jump(int[] nums) {
+        if(nums.length==1)return 0;
+
+        int furthest = -1, cnt = 0, currentEnd = 0;
+
+        for(int i = 0 ; i < nums.length-1 ; i++){
+
+            furthest = Math.max(furthest, i+nums[i]);
+                        
+            if(i==currentEnd){
+                cnt++;
+                currentEnd = furthest;
+            }
+            
+        }
+
+        return cnt;
+    }
+}
