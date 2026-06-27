5f1537b082a32e9526fa905f34c0e598c87ce1f9
Eminence <himanshumaurya447756@gmail.com>
2026-06-27 04:36:36 +0000

    added.

---
+++ HashMap/TwoSum.java
@@ -0,0 +1,27 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int [] ans = new int[2];
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+           int comp = target - nums[i];
+
+           if(map.containsKey(comp)){
+            ans[0] = map.get(comp);
+            ans[1] = i ;
+            break ;
+           }
+
+           map.put(nums[i], i);
+        }
+
+
+        return ans;
+    }
+}
