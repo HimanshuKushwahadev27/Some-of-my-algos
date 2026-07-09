911c7fd91e87ee1c03fe19651b4f9e0ecb1dfea8
Eminence <himanshumaurya447756@gmail.com>
2026-07-09 16:22:30 +0000

    added.

---
+++ BinaryTree/RightSideView.java
@@ -0,0 +1,45 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightSideView {
+       public class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+   public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if(root == null)return ans ;
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size(); 
+            for(int i = 0 ; i < size ; i++){
+                 TreeNode node = q.poll();
+
+                if(i == size - 1){
+                    ans.add(node.val);
+                }
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+        }
+
+        return ans ;
+
+    }
+}
